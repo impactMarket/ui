3e761db09ac9b766e6657cc400302ce340d798df
Paulo Soares <psoares.bj@gmail.com>
2022-02-24 19:08:10 +0000

    Add grid component tweaks

---
+++ dist/esm/types/utils/applyMqProps.d.ts
@@ -0,0 +1,4 @@
+import { FlattenSimpleInterpolation } from 'styled-components';
+import { MqProp } from '..';
+declare const applyMqProps: (prop: MqProp<any>, callback: Function | string | FlattenSimpleInterpolation) => FlattenSimpleInterpolation;
+export default applyMqProps;
