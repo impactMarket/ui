976b00134acd9430fbb06508aec010439b7c2d61
Paulo Soares <psoares.bj@gmail.com>
2022-02-24 19:23:33 +0000

    Remove unnecessary files

--- dist/esm/types/utils/applyMqProps.d.ts
+++
@@ -1,4 +0,0 @@
-import { FlattenSimpleInterpolation } from 'styled-components';
-import { MqProp } from '..';
-declare const applyMqProps: (prop: MqProp<any>, callback: Function | string | FlattenSimpleInterpolation) => FlattenSimpleInterpolation;
-export default applyMqProps;
