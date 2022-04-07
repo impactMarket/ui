2a1b1554d89da240fd470cf614356d6edf793c84
ruifsa <ruifsa4@gmail.com>
2022-04-07 15:58:40 +0100

    Add Label component

---
+++ src/stories/2-base/Elements/Label.stories.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable sort-keys */
+import { Label as BaseLabel, LabelProps } from '../../../components/Label';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { getGeneratedPropArgs, setGeneratedPropArgs } from '../../../helpers/generatedPropArgs';
+import { stateTypes } from '../../../helpers/applyStateColor';
+import React from 'react';
+import base from 'paths.macro';
+import icons from '../../../icons/ui';
+
+export default {
+    argTypes: {
+        icon: {
+            control: { type: 'select' },
+            options: Object.keys(icons)
+        },
+        state: {
+            control: { type: 'radio' },
+            description: 'Pass as bool prop',
+            options: stateTypes
+        },
+        ...setGeneratedPropArgs()
+    },
+    parameters: {
+        generator: true
+    },
+    title: `Components/${base.replace('/src/stories/2-base/', '')}Label`
+} as ComponentMeta<any>;
+
+const Template: ComponentStory<any> = args => {
+    const props = Object.values(args).reduce((results, prop) => ({ ...(results as any), [prop as string]: true }), {
+        ...getGeneratedPropArgs(args)
+    }) as LabelProps;
+
+    return (
+        <>
+            <BaseLabel {...props} />
+        </>
+    );
+};
+
+export const Label = Template.bind({});
+Label.args = {
+    content: 'Test Label',
+    icon: 'arrowRight'
+};
