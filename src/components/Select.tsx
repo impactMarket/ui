741920a82c7149c7c3c00ae4856634576cd52fb5
Paulo Soares <paulo.soares@uphold.com>
2022-04-27 10:50:19 +0100

    Add Select field

---
+++ src/components/Select.tsx
@@ -0,0 +1,246 @@
+import { GeneratedPropTypes } from '../types';
+import { Icon } from './Icon';
+import { colors } from '../theme/colors';
+import { ease, generateProps, transitions } from 'styled-gen';
+import { flyoutMenuShadow, fonts, inputWrapperStyle } from '../theme';
+import { position } from 'polished';
+import { useClickOutside } from '../hooks/useClickOutside';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
+
+type Value = string | number;
+
+type OptionType = {
+    label?: string;
+    value: string;
+};
+
+export type SelectProps = {
+    isMultiple?: boolean;
+    onBlur?: Function;
+    onChange?: Function;
+    onFocus?: Function;
+    options?: OptionType[];
+    optionsSearchPlaceholder?: string;
+    renderLabel: Function;
+    renderOption?: Function;
+    value?: Value | Value[];
+    withError?: boolean;
+    withOptionsSearch?: boolean;
+} & GeneratedPropTypes;
+
+const SelectedOptionContent = styled.div`
+    padding-right: 0.5rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
+
+const InputWrapper = styled.a<{ isFocused?: boolean; withError?: boolean } & GeneratedPropTypes>`
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+
+    ${inputWrapperStyle};
+`;
+
+const Option = styled.a<{ isActive?: boolean }>`
+    display: flex;
+    padding: 0.625rem 0.875rem;
+`;
+
+const OptionsContent = styled.div`
+    overflow-y: auto;
+    max-height: 17.25rem;
+`;
+
+const OptionsWrapper = styled.div<{ isActive?: boolean; withOptionsSearch?: boolean }>`
+    ${transitions('all', 250, ease.inOutCubic)};
+
+    background-color: ${colors.n01};
+    border-radius: 0.5rem;
+    opacity: 0;
+    position: absolute;
+    top: 100%;
+    transform: translateY(1.5rem);
+    visibility: hidden;
+    width: 100%;
+    z-index: 1;
+    padding-top: ${({ withOptionsSearch }) => (withOptionsSearch ? '2.75rem' : 0)};
+
+    ${({ isActive }) =>
+        isActive &&
+        css`
+            opacity: 1;
+            visibility: visible;
+            transform: translateY(0.5rem);
+        `}
+
+    ${flyoutMenuShadow};
+`;
+
+const SearchInput = styled.div`
+    ${position('absolute', 0, 0, null, 0)};
+
+    background-color: ${colors.n01};
+    display: flex;
+    padding: 0.625rem 0.875rem;
+
+    input {
+        color: ${colors.g900};
+        font-family: ${fonts.families.sans};
+        font-size: 1rem;
+        width: 100%;
+
+        ::placeholder,
+        ::-webkit-input-placeholder {
+            color: ${colors.g500};
+        }
+        :-ms-input-placeholder {
+            color: ${colors.g500};
+        }
+    }
+`;
+
+const Wrapper = styled.div`
+    position: relative;
+
+    ${generateProps};
+`;
+
+export const Select = React.forwardRef((props: SelectProps, ref) => {
+    const {
+        isMultiple,
+        onBlur,
+        onChange,
+        onFocus,
+        options,
+        optionsSearchPlaceholder,
+        renderLabel,
+        renderOption,
+        value,
+        withError,
+        withOptionsSearch,
+        ...wrapperProps
+    } = props;
+
+    const [filteredOptions, setIsFilteredOptions] = useState(options || []);
+    const [filterTerm, setFilterTerm] = useState('');
+    const [isActive, setIsActive] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+
+    const optionsListRef = useRef();
+
+    const checkIfActive = (option: OptionType) =>
+        !isMultiple || !Array.isArray(value) ? value === option?.value : (value as Value[]).includes(option?.value);
+
+    const getLabelValue = () => {
+        return !isMultiple
+            ? options?.find(option => option?.value === value)
+            : options?.filter(option => (value as Value[]).includes(option?.value));
+    };
+
+    const handleBlur = useCallback(
+        (event?: any) => {
+            setIsFocused(false);
+
+            if (typeof onBlur === 'function') {
+                return onBlur(event);
+            }
+        },
+        [onBlur]
+    );
+
+    const handleChange = (option: OptionType) => {
+        if (typeof onChange !== 'function') {
+            return;
+        }
+        const { value: optionValue } = option;
+
+        if (!isMultiple) {
+            return onChange(optionValue);
+        }
+
+        const newValueArray = (value as Value[]).includes(optionValue)
+            ? ((value || []) as Value[]).filter(pickedValue => pickedValue !== optionValue)
+            : [...(value as Value[]), optionValue];
+
+        return onChange(newValueArray);
+    };
+
+    const handleFocus = useCallback(
+        (event?: any) => {
+            setIsFocused(true);
+
+            if (typeof onFocus === 'function') {
+                return onFocus(event);
+            }
+        },
+        [onFocus]
+    );
+
+    const handleClickOutside = () => {
+        if (!isActive) {
+            return;
+        }
+
+        return setIsActive(false);
+    };
+
+    useClickOutside(optionsListRef?.current, handleClickOutside);
+
+    useEffect(() => {
+        if (isActive) {
+            handleFocus();
+        }
+
+        if (!isActive) {
+            handleBlur();
+        }
+    }, [isActive]);
+
+    useEffect(() => {
+        if (filterTerm) {
+            const filteredOptions = options?.filter(
+                ({ label, value }) =>
+                    label?.includes(filterTerm.toLowerCase()) || value.toString().includes(filterTerm.toLowerCase())
+            );
+
+            setIsFilteredOptions(filteredOptions || []);
+        } else {
+            setIsFilteredOptions(options || []);
+        }
+    }, [filterTerm]);
+
+    return (
+        <Wrapper {...wrapperProps} ref={ref as any}>
+            <InputWrapper isFocused={isFocused} onClick={() => setIsActive(!isActive)} withError={withError}>
+                <SelectedOptionContent>{renderLabel({ isFocused, selected: getLabelValue() })}</SelectedOptionContent>
+                <Icon g500 icon={isActive ? 'chevronUp' : 'chevronDown'} size={1.25} />
+            </InputWrapper>
+            <OptionsWrapper isActive={isActive} ref={optionsListRef as any} withOptionsSearch={withOptionsSearch}>
+                <OptionsContent>
+                    {withOptionsSearch && (
+                        <SearchInput>
+                            <Icon g500 icon="search" pr={0.5} size={1.5} />
+                            <input
+                                onChange={event => setFilterTerm(event?.target?.value)}
+                                placeholder={optionsSearchPlaceholder || ''}
+                                type="text"
+                                value={filterTerm || ''}
+                            />
+                        </SearchInput>
+                    )}
+                    {filteredOptions.map((option: OptionType, index: any) => (
+                        <Option isActive={checkIfActive(option)} key={index} onClick={() => handleChange(option)}>
+                            {typeof renderOption === 'function'
+                                ? renderOption({ isActive: checkIfActive(option), ...option })
+                                : option?.label || option?.value}
+                        </Option>
+                    ))}
+                </OptionsContent>
+            </OptionsWrapper>
+        </Wrapper>
+    );
+});
+
+Select.displayName = 'Select';
--- src/theme/fx.ts
+++ src/theme/fx.ts
@@ -15,6 +15,11 @@ export const dropdownMenuShadow = css`
     box-shadow: 0 0.063rem 0.125rem ${rgba(colors.g900, 0.4)};
 `;
 
+export const flyoutMenuShadow = css`
+    box-shadow: 0 0.75rem 1rem -0.25rem ${rgba(colors.g900, 0.1)},
+        0 0.25rem 0.375rem -0.125rem ${rgba(colors.g900, 0.05)};
+`;
+
 const placeholderStyle = css`
     color: ${colors.g500};
 `;
@@ -29,7 +34,8 @@ export const inputWrapperStyle = css`
     ${({ isFocused }: any) =>
         isFocused &&
         css`
-            box-shadow: 0 0.125rem 0.0625rem ${rgba(colors.g900, 0.05)}, 0 0 0 1px ${colors.g400};
+            box-shadow: 0 0.125rem 0.0625rem ${rgba(colors.g900, 0.05)}, 0 0 0 0.0625rem ${colors.p300},
+                0 0 0 0.3125rem ${rgba(colors.p500, 0.1)};
         `}
 
     ${({ withError }: any) =>
