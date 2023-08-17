f59939b9b26b8c6584a1221095e3166e8f53aceb
Paulo Sousa <paulo.86.sousa@gmail.com>
2023-08-17 12:08:28 +0100

    Release 1.6.2

--- src/icons/ui.js
+++ src/icons/ui.js
@@ -664,6 +664,17 @@ const ui = {
       },
     ],
   },
+  sheet: {
+    viewbox: '0 0 20 20',
+    paths: [
+      {
+        d: 'M11.667 1.667H5a1.667 1.667 0 0 0-1.667 1.666v13.334A1.667 1.667 0 0 0 5 18.332h10a1.667 1.667 0 0 0 1.667-1.666v-10m-5-5 5 5m-5-5v5h5m-3.334 4.166H6.667m6.666 3.333H6.667M8.333 7.5H6.667',
+        strokeWidth: '2',
+        strokeLinecap: 'round',
+        strokeLinejoin: 'round',
+      },
+    ],
+  },
   smartphone: {
     viewbox: '0 0 16 22',
     paths: [
